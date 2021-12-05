632e93f3484644c25f919cc6a2529be954b5fbc4
jusruizsa <sruizs.jr@gmail.com>
2021-12-05 18:34:28 -0500

    microservicio soporte

---
+++ support_ms/_support/app/Http/Controllers/PQRController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\PQR;
+
+class PQRController extends Controller
+{
+    public function show()
+   {
+
+        $pqr = PQR::all();
+        return $pqr;
+    //    return view('post', [
+    //        'post' => Post::where('slug', '=', $slug)->first()
+    //    ]);
+   }
+
+   public function store(Request $request)
+   {
+       $pqr = new PQR;
+
+       $pqr->message = $request->message;
+       $pqr->response = $request->response;
+       $pqr->id_user = $request->id_user;
+       $pqr->id_trip = $request->id_trip;
+
+       $pqr->save();
+
+       return response()->json(["result" => "ok"], 201);
+   }
+
+   public function destroy($pqrId)
+ {
+     $pqr = Post::find($pqrId);
+     $pqr->delete();
+
+     return response()->json(["result" => "ok"], 200);       
+ }
+
+ public function update(Request $request, $pqrId)
+   {
+       $pqr = PQR::find($pqrId);
+       $pqr->message = $request->message;
+       $pqr->response = $request->response;
+       $pqr->id_user = $request->id_user;
+       $pqr->id_trip = $request->id_trip;
+       $pqr->save();
+
+       return response()->json(["result" => "ok"], 201);       
+   }
+}
